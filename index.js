98959f10bf7e624c8b068fbb4418c1f02c42e90b
Kevin Whinnery <kevin.whinnery@gmail.com>
2014-09-28 22:39:49 -0700

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.post('/send', function(request, response) {
     client.sendMessage({
         to: phone,
         from: '+16129991519',
-        body: 'Thanks for visiting us at JavaOne! Learn more about Twilio: http://bit.ly/twilio_javaone'
+        body: 'Thanks for visiting us at JavaOne! Learn more about Twilio: http://bit.ly/twilio_java_one'
     }, function(err, message) {
         if (err) {
             response.send(500, err);
